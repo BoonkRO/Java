24b2e1abd8ebfaa6792be4bd628009497251004c
Unknown <andrivgar@gmail.com>
2018-04-19 17:55:02 +0200

    Builder Pattern
    parte 1/3

---
+++ BuilderPattern/src/main/java/org/mvpigs/Persona.java
@@ -0,0 +1,57 @@
+package org.mvpigs;
+
+public class Persona {
+
+    private String nombre;
+    private int edad;
+    private String municipio;
+    private String colegio;
+    private String lugarTrabajo;
+
+    private Persona() {}
+
+
+    public static class Builder {
+        private Persona persona;
+
+        public Builder(String nombre) {
+            persona = new Persona();
+            persona.nombre = nombre;
+        }
+
+        public Builder setMunicipio (String municipio) {
+            persona.municipio = municipio;
+            return this;
+        }
+
+        public Builder setMayor(int edad) {
+            if (edad < 18){
+                throw new IllegalArgumentException("es menor de edad " + edad);
+            }
+            persona.edad = edad;
+            persona.colegio = null;
+            return this;
+        }
+
+        public Builder setMenor(int edad) {
+            if (edad >= 18){
+                throw new IllegalArgumentException("es mayor de edad " + edad);
+            }
+            persona.edad = edad;
+            persona.lugarTrabajo = null;
+            return this;
+        }
+
+        public Persona build() {
+
+            return persona;
+        }
+
+        public Builder setLugarTrabajo(String lugarTrabajo) {
+            persona.lugarTrabajo = lugarTrabajo;
+            return this;
+        }
+
+    }
+
+}
